422c9cf612ef30f1b6e4416841604c1e0439ff27
privatePython <gitarist.pasha@gmail.com>
2025-01-07 00:59:12 +0300

    feat: add solution for remove-from-list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+
+  let pointer = l;
+
+  while (pointer.next !== null) {
+    if(pointer.next.value === k) {
+      pointer.next = pointer.next.next
+    } else {
+      pointer = pointer.next
+    }
+  }
+  return l;
+
 }
 
 module.exports = {
